0db6a2f849342c34e6cefec945623bc004052656
ronin010 <user_rh99@protonmail.com>
2020-09-19 16:54:23 +0100

    initial commit

---
+++ audio/james/Files.js
@@ -0,0 +1,57 @@
+export const Files = [
+  {
+    id: 1,
+    title: "yeah - 1",
+    clip: require("./yeah-1.mp3"),
+    image: require("../../assets/james-yeah-1.png")
+  },
+  {
+    id: 2,
+    title: "yeah - 2",
+    clip: require("./yeah-2.mp3"),
+    image: require("../../assets/james-yeah-2.png")
+  },
+  {
+    id: 3,
+    title: "mommy",
+    clip: require("./mommy.mp3"),
+    image: require("../../assets/mommy.png")
+  },
+  {
+    id: 4,
+    title: "yeah haa",
+    clip: require("./yeaha.mp3"),
+    image: require("../../assets/yeaha.png")
+  },
+  {
+    id: 5,
+    title: "hey buddy",
+    clip: require("./buddy.mp3"),
+    image: require("../../assets/buddy.png")
+  },
+  {
+    id: 6,
+    title: "ah no",
+    clip: require("./ah-no.mp3"),
+    image: require("../../assets/ah-no.png")
+  },
+  {
+    id: 7,
+    title: "oooaaahhh",
+    clip: require("./ahh.mp3"),
+    image: require("../../assets/ahh.png")
+  },
+  {
+    id: 8,
+    title: "your choice",
+    clip: require("./choice.mp3"),
+    image: require("../../assets/choice.png")
+  },
+  {
+    id: 9,
+    title: "excuse me",
+    clip: require("./burp.mp3"),
+    image: require("../../assets/burp.png")
+  }
+]
+
---
+++ audio/kirk/Files.js
@@ -0,0 +1,56 @@
+export const Files = [
+  {
+    id: 1,
+    title: "Wah - 1",
+    clip: require("./wah-1.mp3"),
+    image: require("../../assets/kirk-wah-1.png")
+  },
+  {
+    id: 2,
+    title: "Wah - 2",
+    clip: require("./wah-2.mp3"),
+    image: require("../../assets/kirk-wah-2.png")
+  },
+  {
+    id: 3,
+    title: "wah - 3",
+    clip: require("./wah-3.mp3"),
+    image: require("../../assets/kirk-wah-3.png")
+  },
+  {
+    id: 4,
+    title: "wah - 4",
+    clip: require("./wah-4.mp3"),
+    image: require("../../assets/kirk-wah-4.png")
+  },
+  {
+    id: 5,
+    title: "i'm your pain",
+    clip: require("./pain.mp3"),
+    image: require("../../assets/kirk-pain.png")
+  },
+  {
+    id: 6,
+    title: "I got that",
+    clip: require("./got-that.mp3"),
+    image: require("../../assets/hard-riff.png")
+  },
+  {
+    id: 7,
+    title: "fit in my van",
+    clip: require("./van.mp3"),
+    image: require("../../assets/van.png")
+  },
+  {
+    id: 8,
+    title: "goat skin boots",
+    clip: require("./goat.mp3"),
+    image: require("../../assets/goat.png")
+  },
+  {
+    id: 9,
+    title: "my apocalypse",
+    clip: require("./apocalypse.mp3"),
+    image: require("../../assets/apocalypse.png")
+  }
+]
